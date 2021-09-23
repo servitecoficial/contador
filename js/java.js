7aa87479f5ec098fc40b79db2faa8ac734250ac8
servitecoficial <77131260+servitecoficial@users.noreply.github.com>
2021-09-23 20:30:12 -0300

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,7 @@
+var botonElement = document.getElementById("botonClick");
+var pElement = document.getElementById("areaContador");
+var contador = 0;
+botonElement.onclick = function () {
+    contador++;
+    pElement.textContent = contador;
+}
